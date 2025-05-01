e28004687c150634a01101cf4795dedf6ceb5ae7
Kazuki Takada <kazu0105barcafan@icloud.com>
2025-05-01 20:55:54 +0900

    5-intersectionsを作成

--- config/config.py
+++ config/config.py
@@ -7,25 +7,25 @@ class Config(Object):
 
         self.readConfigFile()
         self.readRoads()
+        self.readIntersections()
     
     def readConfigFile(self):
         with open('layout/config.yaml', 'r') as file:
             data = yaml.safe_load(file)
-
             self.network_name = data['simulator']['network_name']
             self.simulation_time = data['simulator']['simulation_time']
 
     def readRoads(self):
         with open('layout/' + self.network_name + '/roads.yaml', 'r') as file:
-            self.roads = yaml.safe_load(file)
+            data = yaml.safe_load(file)
+            self.roads = data['roads']
+            self.road_link_tags = data['road_link_tags']
 
-            for road_id in range(len(self.roads)):
-                road = self.roads[road_id]
-                intersection_ids = road['intersection_ids']
-                road.pop('intersection_ids')
-                road['intersections'] = {'input': intersection_ids[0], 'output': intersection_ids[1]}
-
-            print(self.roads)
+    def readIntersections(self):
+        with open('layout/' + self.network_name + '/intersections.yaml', 'r') as file:
+            data = yaml.safe_load(file)
+            self.intersections = data['intersections']
+            self.intersection_road_tags = data['intersection_road_tags']
             
 
 
