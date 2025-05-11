2f2f862caf0f2d13f51e8095245ee1c5db724a13
Kazuki Takada <kazu0105barcafan@icloud.com>
2025-05-11 19:31:51 +0900

    28-設定ファイルに制御手法のデータを加えた

--- config/config.py
+++ config/config.py
@@ -4,10 +4,13 @@ from libs.common import Common
 
 class Config(Common):
     def __init__(self):
+        # 継承
         super().__init__()
 
+        # config.yamlを読み込む
         self.readConfigFile()
 
+        # ネットワークのパラメータをcsvから読み込む
         self.roads = pd.read_csv('layout/' + self.network_name + '/roads.csv')
         self.road_link_tags = pd.read_csv('layout/' + self.network_name + '/road_link_tags.csv')
         self.intersections = pd.read_csv('layout/' + self.network_name + '/intersections.csv')
@@ -15,12 +18,14 @@ class Config(Common):
         self.link_input_tags = pd.read_csv('layout/' + self.network_name + '/link_input_tags.csv')
         self.intersection_turn_ratio_tags = pd.read_csv('layout/' + self.network_name + '/intersection_turn_ratio_tags.csv')
 
+        # 旋回率のテンプレートを取得する
         self.getNumRoadTurnRatioMap()
     
     def readConfigFile(self):
         with open('layout/config.yaml', 'r') as file:
             data = yaml.safe_load(file)
             self.network_name = data['simulator']['network_name']
+            self.control_method = data['simulator']['control_method']
             self.simulation_time = data['simulator']['simulation_time']
 
     def getNumRoadTurnRatioMap(self):
