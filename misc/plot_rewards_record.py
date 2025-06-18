5d7a2beb1d891cfddb7cb3b6c62d76f4b9c40b5e
Kazuki Takada <kazu0105barcafan@icloud.com>
2025-06-18 16:58:29 +0900

    66-報酬を変更

--- misc/plot_rewards_record.py
+++ misc/plot_rewards_record.py
@@ -3,11 +3,18 @@ import matplotlib.pyplot as plt
 
 rewards_record = np.load('results/rewards_record1_3333_10.npy', allow_pickle=True).tolist()
 
-plt.plot(rewards_record)
-plt.xlabel('Episode')
-plt.ylabel('Reward')
-plt.title('Rewards Over Episodes')
+plt.plot(rewards_record, linewidth=2)
+plt.xlabel('Episode', fontsize=14)
+plt.ylabel('Reward', fontsize=14)
+plt.title('Rewards Over Episodes', fontsize=16)
 plt.grid()
+
+# 線を太くする
+plt.tick_params(axis='both', which='major', labelsize=14)
+plt.tick_params(axis='both', which='minor', labelsize=14)
+plt.xticks(fontsize=14)
+plt.yticks(fontsize=14)
+
 plt.show()
 
 print('test')
