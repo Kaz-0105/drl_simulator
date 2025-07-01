33f5ccc9ead8ca97b8ae45a1f12a359ed824e0e3
Kazuki Takada <kazu0105barcafan@icloud.com>
2025-07-02 01:58:55 +0900

    94-計算時間を出そうとした

--- misc/plot_metric_bar.py
+++ misc/plot_metric_bar.py
@@ -12,9 +12,11 @@ row_names = ['drl', 'mpc']
 
 max_queue_list = []
 average_queue_list = []
+calc_time_list = []
 for row_idx, row_simulation_list in enumerate(simulation_list):
     tmp_max_queue_list = []
     tmp_average_queue_list = []
+    tmp_calc_time_list = []
     for col_idx, simulation_id in enumerate(row_simulation_list):
         metric_path = Path(f"results/metrics/metric_{simulation_id}.pkl")
         if not metric_path.exists():
@@ -31,43 +33,66 @@ for row_idx, row_simulation_list in enumerate(simulation_list):
                 average_queue_record = saved_data['average_queue']
                 tmp_average_queue_list.append(average_queue_record['queue_length'].mean())
             
+            if simulation_id == 10:
+                print('test')
+            if saved_data['calc_time'] is not None:
+                calc_time_record = saved_data['calc_time']
+                tmp_calc_time_list.append(calc_time_record['calculation_time'].mean())
+            
     max_queue_list.append(tmp_max_queue_list)
     average_queue_list.append(tmp_average_queue_list)
+    calc_time_list.append(tmp_calc_time_list)
 
 fig_max_queue, ax_max_queue = plt.subplots()
 fig_average_queue, ax_average_queue = plt.subplots()
+fig_calc_time, ax_calc_time = plt.subplots()
 
 # 最大キュー長について
 x = np.arange(len(simulation_list[0]))
 for row_idx, row_max_queue_list in enumerate(max_queue_list):
     ax_max_queue.bar(
-        [x + row_idx * 0.15 for x in range(len(row_max_queue_list))],
+        [x + row_idx * 0.2 for x in range(len(row_max_queue_list))],
         row_max_queue_list,
-        width=0.15,
+        width=0.2,
         label=row_names[row_idx]
     )
 
-ax_max_queue.set_xticks(x + 0.15, column_names)
-ax_max_queue.set_title('Average Max Queue Length')
-ax_max_queue.set_xlabel('Simulation Configuration')
-ax_max_queue.set_ylabel('Average Max Queue Length (m)')
-ax_max_queue.legend()
-ax_max_queue.grid()
+ax_max_queue.set_xticks(x + 0.1)
+ax_max_queue.set_xticklabels(column_names, fontsize=14)
+ax_max_queue.set_title('Average Max Queue Length', fontsize=16)
+ax_max_queue.set_xlabel('Simulation Configuration', fontsize=14)
+ax_max_queue.set_ylabel('Average Max Queue Length (m)', fontsize=14)
+ax_max_queue.legend(fontsize=14)
 
 # 平均キュー長について
 for row_idx, row_average_queue_list in enumerate(average_queue_list):
     ax_average_queue.bar(
-        [x + row_idx * 0.15 for x in range(len(row_average_queue_list))],
+        [x + row_idx * 0.2 for x in range(len(row_average_queue_list))],
         row_average_queue_list,
-        width=0.15,
+        width=0.2,
         label=row_names[row_idx]
     )
-ax_average_queue.set_xticks(x + 0.15, column_names)
-ax_average_queue.set_title('Average Queue Length')
-ax_average_queue.set_xlabel('Simulation Configuration')
-ax_average_queue.set_ylabel('Average Queue Length (m)')
-ax_average_queue.legend()
-ax_average_queue.grid()
+ax_average_queue.set_xticks(x + 0.1)
+ax_average_queue.set_xticklabels(column_names, fontsize=14)
+ax_average_queue.set_title('Average Queue Length', fontsize=16)
+ax_average_queue.set_xlabel('Simulation Configuration', fontsize=14)
+ax_average_queue.set_ylabel('Average Queue Length (m)', fontsize=14)
+ax_average_queue.legend(fontsize=14)
+
+# 計算時間について
+for row_idx, row_calc_time_list in enumerate(calc_time_list):
+    ax_calc_time.bar(
+        [x + row_idx * 0.2 for x in range(len(row_calc_time_list))],
+        row_calc_time_list,
+        width=0.2,
+        label=row_names[row_idx]
+    )
+ax_calc_time.set_xticks(x + 0.1)
+ax_calc_time.set_xticklabels(column_names, fontsize=14)
+ax_calc_time.set_title('Calculation Time', fontsize=16)
+ax_calc_time.set_xlabel('Simulation Configuration', fontsize=14)
+ax_calc_time.set_ylabel('Calculation Time (s)', fontsize=14)
+ax_calc_time.legend(fontsize=14)
 
 plt.show()
 
