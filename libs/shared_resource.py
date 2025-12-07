f0737e4b783e3418b636e4e262a6eb5de4c91cd8
Kazuki Takada <kazu0105barcafan@icloud.com>
2025-12-07 23:25:46 +0900

    109-バグを解消

--- libs/shared_resource.py
+++ libs/shared_resource.py
@@ -1,8 +1,11 @@
 from libs.common import Common
 
 class SharedResources(Common):
-    def __init__(self):
+    def __init__(self, vissim):
         super().__init__()
+
+        self.vissim = vissim
+        return
 
 
 
