b89dd170a06d6eca2ba75de8bae50f04638e8489
Kazuki Takada <takada.k.culer@gmail.com>
2026-01-12 20:06:54 +0100

    119-フィギュアをアップデート

--- misc/check_weight_difference.py
+++ misc/check_weight_difference.py
@@ -22,7 +22,7 @@ config = {
         'balanced' : True,
         'balanced_high' : True,
         'unbalanced' : True,
-        'main-minor' : False,
+        'main-minor' : True,
     },
     'compare_to' : 'all' # 1. 'all': 全てのdemand_typeを1つのグラフで比較, 2. 'each': 各demand_typeごとに比較（scootとdrlの結果も基準線として表示）
 }
@@ -33,8 +33,8 @@ weights = {
         'balanced_low': [6, 8, 10, 12, 14, 16, 18, 20],
         'balanced' : [6, 8, 10, 12, 14, 18, 20],
         'balanced_high' : [6, 8, 10, 12, 14, 16, 18, 20],
-        'unbalanced' : [6],
-        'main-minor' : [],
+        'unbalanced' : [6, 8, 10, 12, 14, 16, 20],
+        'main-minor' : [8, 10, 12, 14, 16],
     },
     '3333' : {
         'balanced_low': [],
