57dd55fbe7e413f4d6550d2fd4980a182bb2fd5f
Kazuki Takada <takada.k.culer@gmail.com>
2026-01-12 15:39:49 +0100

    119-実験を保存

--- misc/check_weight_difference.py
+++ misc/check_weight_difference.py
@@ -20,7 +20,8 @@ config = {
     'inflow_types': {
         'balanced_low': True,
         'balanced' : True,
-        'unbalanced' : False,
+        'balanced_high' : True,
+        'unbalanced' : True,
         'main-minor' : False,
     },
     'compare_to' : 'all' # 1. 'all': 全てのdemand_typeを1つのグラフで比較, 2. 'each': 各demand_typeごとに比較（scootとdrlの結果も基準線として表示）
@@ -30,8 +31,9 @@ config = {
 weights = {
     '2222' : {
         'balanced_low': [6, 8, 10, 12, 14, 16, 18, 20],
-        'balanced' : [6, 8],
-        'unbalanced' : [],
+        'balanced' : [6, 8, 10, 12, 14, 18, 20],
+        'balanced_high' : [6, 8, 10, 12, 14, 16, 18, 20],
+        'unbalanced' : [6],
         'main-minor' : [],
     },
     '3333' : {
